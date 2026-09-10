9afbab2f38d3877929461c01b619ab6930bc6ab2
brochj <brochj@gmail.com>
2026-09-10 16:04:12 -0300

    primeira versão do site, sem atualizar o telefone

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const WHATSAPP_URL = 'https://wa.me/SEUNUMERO';
+export const INSTAGRAM_USER = 'broksolucoeseletricas';
+export const INSTAGRAM_URL = `https://www.instagram.com/${INSTAGRAM_USER}`
+export const EMAIL = 'broksolucoeseletricas@gmail.com';
